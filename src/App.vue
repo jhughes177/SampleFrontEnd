548facf3d70e4436960aba535289dcdc9f13f862
Jared Hughes <jhugh870@mail.endicott.edu>
2021-04-12 20:10:23 -0400

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <Emailing msg="Welcome to Your Vue.js App"/>
+</template>
+
+<script>
+import Emailing from './components/Emailing.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Emailing
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/Emailing.vue
@@ -0,0 +1,53 @@
+<template>
+<div>
+ <div id="emailing">
+  <h1>MassMutual Coding Sample</h1>
+  <br>
+  <p>Please ensure you have the NodeJS emailing API setup locally</p>
+  <br>
+  <form id="signup" method="post" v-on:submit.prevent>
+  <input v-model="fromEmail" placeholder="From Email" required/><br>
+  <input v-model="toEmail" placeholder="To Email" required /><br>
+  <input v-model="emailSubject" placeholder="Subject" required/><br>
+  <input v-model="emailText" placeholder="Email Text" required/><br>
+  <br>
+  <button v-on:click="sendEmail" type="submit">Send Email</button>
+  </form>
+</div>
+</div>
+</template>
+<script>
+export default {
+  name: 'Emailing',
+  methods: {
+    sendEmail: function () {
+      this.request = {
+        from: this.fromEmail,
+        to: this.toEmail,
+        subject: this.emailSubject,
+        text: this.emailText
+      };
+      console.log(this.request);
+  if(this.fromEmail && this.toEmail) { 
+    return fetch('http://{replaceWithLocalURI}/sendemail', { //Use local URL for testing NodeJS api ex
+      method: 'post',
+      mode: 'cors',
+      headers: {
+        'Accept': 'application/json',
+        'Content-type' : 'application/json'
+      },
+      body: JSON.stringify(this.request)
+        }).then(result => {
+          console.log(result);
+          if(result){
+          alert('Email Sent to: ' + this.request.to + '!')
+          }
+      });
+  } else{
+    alert('Please provide From and To Email Addresses')
+  }    
+ 
+    }
+  }
+}
+</script>
